cdbe89ae225e87ae0b84baa41edf938955c904db
SarKhann <serxan.babayev.06@gmail.com>
2025-05-16 01:10:14 +0400

    Initial commit

---
+++ src/data/items.js
@@ -0,0 +1,12 @@
+const themeColor = '#407BFF'
+const siteName = 'AuxCarSoft'
+
+const contactInfo = {
+    address: 'Springfield,742 Evergreen Terrace',
+    phone1: '+994 50 201 71 64',
+    phone2: '+994 55 206 71 35',
+    email: 'auxcarsoft@gmail.com',
+    website: 'https://auxcarsoft.com'
+}
+
+export { themeColor, siteName, contactInfo }
